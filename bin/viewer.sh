4bf383ddf77918348f23c958e7ddb06cc3d640c6
zeroway <M_O_Bz@163.com>
2021-03-24 16:22:51 +0800

    Ignore files under git directory

--- bin/viewer.sh
+++ bin/viewer.sh
@@ -6,7 +6,7 @@ file_viewer()
 	xclip -h >/dev/null || exit
 
 	# open the specifiy file
-	chosen=$(find $1 -type f | rofi -dmenu -i -l 20 -width 50 -fn Monospace-18)
+	chosen=$(find $1 -path "$HOME/.grice/.git" -prune -o -type f | rofi -dmenu -i -l 20 -width 50 -fn Monospace-18)
 	[ "$chosen" != "" ] || exit
 	# set global variabl TERMINAL and EDITOR in bashrc or profile file
 	setsid -f "$TERMINAL" -e "$EDITOR" "$chosen"  >/dev/null 2>&1
